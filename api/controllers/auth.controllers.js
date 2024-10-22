f0f79ee27a4b9c127ec7251acbc58aec08fad7e0
Tushar Verma <verma071276@gmail.com>
2024-10-22 22:55:05 +0530

    create signup api

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,7 +1,6 @@
 import User from '../models/user.models.js';
 import bcryptjs  from 'bcryptjs';
 
-
 export const signup = async (req,res) =>{
     const  {username,email,password} = req.body;
     const hashedPassword =  bcryptjs.hashSync(password,10);
