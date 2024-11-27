81136c3b0395be45c20b3e73045e8d74d71eca36
Tushar Verma <verma071276@gmail.com>
2024-11-27 22:19:17 +0530

    Create Signin API Route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import User from '../models/user.models.js';
 import bcryptjs  from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res,next) =>{
     const  {username,email,password} = req.body;
@@ -10,5 +11,23 @@ export const signup = async (req,res,next) =>{
         res.status(201).json('User Created Successfully');
     }catch(error){
         next(error);
+    }
+}
+
+export const signin = async (req,res,next) =>{
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User Not Found'));
+        const isValidPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!isValidPassword) return next(errorHandler(401, 'Invalid Password'));
+        const token = jwt.sign({id:validUser._id},process.env.JWT)
+        const {password: pass,...rest} = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true})
+        res.status(200).json(rest);
+    }
+
+    catch (error){
+        
     }
 }
